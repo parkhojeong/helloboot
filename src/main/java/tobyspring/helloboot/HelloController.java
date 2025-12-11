3e09683f60cbaf993cb8e41a7635fb269bc623f9
parkhojeong <parkhj062@gmail.com>
2025-12-12 02:52:23 +0900

    Component scan

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,11 +1,13 @@
 package tobyspring.helloboot;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
+@MyComponent
 public class HelloController {
     private final HelloService helloService;
 
