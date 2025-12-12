1c5bc714484ed15dfa4eebd533b1b8bf5ab4a86e
hj <parkhj062@gmail.com>
2025-12-12 21:24:59 +0900

    HelloApiTest

---
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,27 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class HelloApiTest {
+    @Test
+    void helloApi() {
+        // http localhost:8080/hello?name=Spring
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // status 200
+        Assertions.assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header(content-type) text/plain
+        Assertions.assertThat(res.getHeaders().getContentType().toString()).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        // body Hello Spring
+        Assertions.assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+}
